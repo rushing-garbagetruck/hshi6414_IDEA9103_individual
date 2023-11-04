0364b12b7213c7c8e3a16276cda6d441775ac97e
rushing-garbagetrunk <hshi6414@uni.sydney.edu.au>
2023-11-05 01:40:51 +1100

    Patterns spinning while music playing.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
 // Global variables to store circle patterns, circle diameter, and spacing between circles
+let song;
+let fft;
 let patterns = [];
 let circleDiameter;
 let spacing = 30; // Define space between circles
+let angle = 0; // Global variable to control the rotation angle
 
+function preload() {
+  song = loadSound('audio/Bless_This_Space.mp3', soundLoaded, loadError);
+}
+
+function soundLoaded() {
+  console.log('Sound loaded successfully');
+}
+
+function loadError(err) {
+  console.log('Error loading sound: ' + err);
+}
 
 function setup() {
   // Create a canvas to fit the full window size and set the background color
   createCanvas(windowWidth, windowHeight);
   background('#194973');
-  noLoop(); // Prevent p5.js from continuously redrawing the canvas
 
   circleDiameter = 200; // Define a fixed diameter for the circles
 
@@ -43,6 +56,7 @@ function setup() {
       });
     }
   }
+  
 
   // Draw each circle pattern on the canvas
   for(let pattern of patterns) {
@@ -50,10 +64,30 @@ function setup() {
   }
 }
 
+// Draw animation
+function draw() {
+  background('#194973'); // Clear the canvas
 
-// Function to draw an individual circle pattern
+  // Check if the song is playing before updating the angle for rotation
+  if (song.isPlaying()) {
+    angle += 0.01; // Increase the angle for rotation only if the song is playing
+  }
+
+  // Draw each circle pattern with the current angle
+  for (let pattern of patterns) {
+    push(); // Save the current drawing state
+    translate(pattern.x, pattern.y); // Move the origin to the circle's center
+    if (song.isPlaying()) {
+      rotate(angle); // Rotate the grid only if the song is playing
+    }
+    drawPattern(pattern); // Draw the pattern
+    pop(); // Restore the original drawing state
+  }
+}
+
+
+
 function drawPattern(pattern) {
-
   // Draw the outer "pearl necklace" chain around each circle with the new pattern
   let outerRadius = pattern.size / 2 + 10; // Define the radius for the pearl chain
   let pearls = [1, 1, 1, 0]; // Define the pattern of pearls (1 small, 1 small, 1 small, 0 large, and so on)
@@ -62,11 +96,11 @@ function drawPattern(pattern) {
   let numPearls = TWO_PI * outerRadius / 20;
   for (let i = 0; i < numPearls; i++) {
     let angle = i * TWO_PI / numPearls;
-    let pearlX = pattern.x + outerRadius * cos(angle);
-    let pearlY = pattern.y + outerRadius * sin(angle);
+    let pearlX = outerRadius * cos(angle);
+    let pearlY = outerRadius * sin(angle);
 
     if (pearls[pearlIndex] === 1) {
-      fill(random(255), random(255), random(255)); // Set the fill color for the small pearls
+      fill(pattern.dotColor); // Set the fill color for the small pearls
       ellipse(pearlX, pearlY, 10); // Draw a small pearl
     } else {
       fill(255); // Set the fill color for the large pearls
@@ -75,59 +109,80 @@ function drawPattern(pattern) {
 
     pearlIndex = (pearlIndex + 1) % pearls.length; // Move to the next pattern element
   }
-    
+
   let numCircle = 5; // Number of circles
   let startRadius = 100; // Initial radius
   let radiusStep = 20; // Decreasing radius
   for(let i = 0; i < numCircle; i++){
     let radius = startRadius - radiusStep * i;
-    ellipse(pattern.x, pattern.y, radius * 2);
-    fill(random(255), random(255), random(255)); // Set the fill color for the circle
+    ellipse(0, 0, radius * 2);
+    fill(pattern.color); // Set the fill color for the circle
   }
-  
+
   let numShapes = 20;
   for(let i = 0; i < numShapes; i++) {
     for(let j = 0; j < 5; j++){
       let angle = TWO_PI / numShapes * i;
-      let shapeX = pattern.x + (pattern.size / 2 - 10 * j) * cos(angle);
-      let shapeY = pattern.y + (pattern.size / 2 - 10 * j) * sin(angle);
+      let shapeX = (pattern.size / 2 - 10 * j) * cos(angle);
+      let shapeY = (pattern.size / 2 - 10 * j) * sin(angle);
       fill(pattern.dotColor); // Set the fill color for the inner shapes
 
       // Depending on the design type, draw either dots, lines, or rings
       if (pattern.type === 0) {
-        // Draw five small circles of radius 5 inside each circle and arrange them neatly
         ellipse(shapeX, shapeY, 5);
 
       } else if(pattern.type === 1) {
-        for(let j = 0; j < 5; j ++){
+        for(let i = 0; i < numShapes; i++) {
           let angle = TWO_PI / numShapes * i;
-          let shapeX1 = pattern.x + (pattern.size / 2 * 0.6 - 10 * j) * cos(angle);
-          let shapeY1 = pattern.y + (pattern.size / 2 * 0.6 - 10 * j) * sin(angle);
-          fill(pattern.dotColor); // Set the fill color for the inner shapes
-          ellipse(shapeX1, shapeY1, 5);
-        }
-        
-        for(let j = 0; j < 5; j ++){
-          let angle = TWO_PI / numShapes * i;
-          let shapeX2 = pattern.x + (pattern.size / 2 - 5 * j) * cos(angle);
-          let shapeY2 = pattern.y + (pattern.size / 2 - 5 * j) * sin(angle);
-          fill(pattern.dotColor); // Set the fill color for the inner shapes
-          ellipse(shapeX2, shapeY2, 5);
+          let innerRadius = pattern.size / 2 * 0.6;
+          for(let j = 0; j < 5; j++){
+            let shapeX = innerRadius * cos(angle) - 10 * j * cos(angle);
+            let shapeY = innerRadius * sin(angle) - 10 * j * sin(angle);
+            fill(pattern.dotColor);
+            ellipse(shapeX, shapeY, 5);
+          }
         }
 
       } else if(pattern.type === 2) {
-        for(let j = 0; j < 8; j ++){
+        for(let j = 0; j < 8; j++){
           let radius = 6 * j;
           noFill();
-          stroke(random(255), random(255), random(255)); // Set the colour of the internal shape stroke
-          ellipse(pattern.x, pattern.y, radius);
+          stroke(pattern.dotColor); // Set the stroke color for the rings
+          ellipse(0, 0, radius); // Draw the rings centered around the new origin (0,0)
         }
-        stroke(0); // Restore stroke colour
-        drawSawtoothRing(pattern.x, pattern.y, pattern.size /3, 20, pattern.size/2*0.35);
+        stroke(0); // Reset the stroke color for other shapes
+      
+        // Now draw the sawtooth ring, still around (0,0)
+        drawSawtoothRing(0, 0, pattern.size / 3, 20, pattern.size / 2 * 0.35);
       }
     }
   }
+
+  if (pattern.type === 2) {
+    drawSawtoothRing(0, 0, pattern.size / 3, 20, pattern.size / 2 * 0.35);
+  }
 }
+
+// The drawSawtoothRing function should also be updated to draw around the origin
+function drawSawtoothRing(cx, cy, radius, teeth, toothHeight){
+  let angleIncrement = TWO_PI / teeth;
+  beginShape();
+  for (let i = 0; i < teeth; i++) {
+    let angle = i * angleIncrement;
+    
+    // Inner vertex
+    let innerX = (radius - toothHeight) * cos(angle);
+    let innerY = (radius - toothHeight) * sin(angle);
+    vertex(innerX, innerY);
+    
+    // Outer vertex
+    let outerX = (radius + toothHeight) * cos(angle + angleIncrement / 2);
+    let outerY = (radius + toothHeight) * sin(angle + angleIncrement / 2);
+    vertex(outerX, outerY);
+  }
+  endShape(CLOSE);
+}
+
 
 
 // Function to handle window resizing
@@ -136,6 +191,7 @@ function windowResized() {
   background('#194973'); // Clear the canvas with the same background
   setup(); // Recalculate columns and rows
   patterns = []; // Reset Pattern Array
+  loop();
 }
 
 
@@ -161,4 +217,12 @@ function drawSawtoothRing(cx, cy, radius, teeth, toothHeight){
 
   endShape(CLOSE);
   fill(random(255), random(255), random(255)); // Restore fill properties of other shapes
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    song.pause(); // Pause the song if it is playing
+  } else {
+    song.loop(); // Play the song if it is not playing
+  }
 }
